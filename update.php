eb77eaea6a1050476873432cbc918679d139768a
emr0913 <136888469+emr0913@users.noreply.github.com>
2024-02-06 18:48:39 +0100

    Update update.php

--- update.php
+++ update.php
@@ -5,8 +5,8 @@ $latestVersion = "1.1";   // Latest version available
 if ($latestVersion > $currentVersion) {
     $updateInfo = array(
         'version' => $latestVersion,
-        'download_url' => 'https://github.com/yourusername/yourrepository/releases/latest',
-        'changelog' => 'Add your changelog here.'
+        'download_url' => 'https://github.com/emr0913/my-note-app-updates/releases/tag/latest',
+        'changelog' => 'test'
     );
 
     echo json_encode($updateInfo);
